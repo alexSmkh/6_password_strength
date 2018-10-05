67d5cffe19e8ea26ba3e94d143e0e0644122c25e
alexSmkh <alexshikari9333@gmail.com>
2018-10-05 19:57:45 +0300

    added functions sum()/getpass(); deleted magic digits; fixed README; simplified functions for checking

--- password_strength.py
+++ password_strength.py
@@ -1,4 +1,5 @@
 import re
+import getpass
 
 
 def print_password_strength(strength):
@@ -6,45 +7,40 @@ def print_password_strength(strength):
 
 
 def get_password():
-    user_password = input('Введите пароль: ')
+    user_password = getpass.getpass('Введите пароль: ')
     return user_password
 
 
 def check_for_upper_case(password_for_check):
-    if re.search(r'[A-ZА-Я]', password_for_check):
-        return 2
-    else:
-        return 0
+    return bool(re.search(r'[A-ZА-Я]', password_for_check))
 
 
 def check_for_lower_case(password_for_check):
-    if re.search(r'[a-zа-я]', password_for_check):
-        return 1
-    else:
-        return 0
+    return bool(re.search(r'[a-zа-я]', password_for_check))
 
 
 def check_for_special_character(password_for_check):
-    if re.search(r'[\W_]', password_for_check):
-        return 2
-    else:
-        return 0
+    return bool(re.search(r'[\W_]', password_for_check))
+
+
+def check_for_digit(password_for_check):
+    return bool(re.search(r'[\d]', password_for_check))
 
 
 def check_length(password_for_check):
-    if len(password_for_check) >= 12:
-        return 3
-    elif len(password_for_check) >= 8:
-        return 2
-    else:
-        return 0
+    recommended_length_password = 8
+    return bool(len(password_for_check) >= recommended_length_password)
 
 
 if __name__ == '__main__':
     password = get_password()
     password_strength = 0
-    password_strength += check_length(password)
-    password_strength += check_for_lower_case(password)
-    password_strength += check_for_upper_case(password)
-    password_strength += check_for_special_character(password)
-    print_password_strength(password_strength)
+    multiplier_to_estimate_password_complexity = 2
+    password_strength = sum([
+        check_length(password),
+        check_for_lower_case(password),
+        check_for_upper_case(password),
+        check_for_special_character(password),
+        check_for_digit(password)]
+    )
+    print_password_strength(password_strength * multiplier_to_estimate_password_complexity)
