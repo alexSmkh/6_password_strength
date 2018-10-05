1155038dc4b0d57dc9509c09e75d7a7eb21df6d4
alexSmkh <alexshikari9333@gmail.com>
2018-10-05 13:32:04 +0300

    simplified functions

--- password_strength.py
+++ password_strength.py
@@ -10,30 +10,41 @@ def get_password():
     return user_password
 
 
-def get_password_strength(password_for_check):
-    strength = 0
-
-    if len(password_for_check) >= 12:
-        strength += 3
-    elif len(password_for_check) >= 8:
-        strength += 2
-
+def check_for_upper_case(password_for_check):
     if re.search(r'[A-ZА-Я]', password_for_check):
-        strength += 2
+        return 2
+    else:
+        return 0
 
+
+def check_for_lower_case(password_for_check):
     if re.search(r'[a-zа-я]', password_for_check):
-        strength += 1
+        return 1
+    else:
+        return 0
 
-    if re.search(r'[0-9]', password_for_check):
-        strength += 2
 
+def check_for_special_character(password_for_check):
     if re.search(r'[\W_]', password_for_check):
-        strength += 2
+        return 2
+    else:
+        return 0
 
-    return strength
+
+def check_length(password_for_check):
+    if len(password_for_check) >= 12:
+        return 3
+    elif len(password_for_check) >= 8:
+        return 2
+    else:
+        return 0
 
 
 if __name__ == '__main__':
     password = get_password()
-    password_strength = get_password_strength(password)
+    password_strength = 0
+    password_strength += check_length(password)
+    password_strength += check_for_lower_case(password)
+    password_strength += check_for_upper_case(password)
+    password_strength += check_for_special_character(password)
     print_password_strength(password_strength)
